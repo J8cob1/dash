3a9f16f3251abe411af3f5b605a06353770c9c27
J8cob1 <J8cob1@users.noreply.github.com>
2020-07-14 16:43:15 -0700

    Did some more work on the calendar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-<<<<<<< Updated upstream
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 import 'bootstrap/dist/css/bootstrap.css'
-=======
 import CalendarWidget from './CalendarWidget';
->>>>>>> Stashed changes
 
 function App() {
   return (
--- src/CalendarWidget.js
+++ src/CalendarWidget.js
@@ -2,14 +2,77 @@ import React from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
-function CalendarWidget() {
-  return (
-    <div className="Calendar">
-      <Calendar/>
-      <div className="notifications">
-      </div>  
-    </div>
-  );
+class CalendarWidget extends React.Component {
+  // For scoping reasons
+  // https://stackoverflow.com/questions/52894546/cannot-access-state-inside-function
+  thisPointer = this;
+
+  // State
+  constructor() {
+    // Call parent constructor
+    super()
+
+    // Set initial state
+    this.state = {
+      date: "",
+      events: []
+    };
+
+    // Bind this to the getCalendarEvents function
+    // https://stackoverflow.com/questions/52894546/cannot-access-state-inside-function
+    this.getCalendarEvents = this.getCalendarEvents.bind(this);
+  }
+
+  getCalendarEvents(selectedDate) {
+    // Get calendar events for the selected day
+    // https://stackoverflow.com/questions/22876978/loop-inside-react-jsx?page=1&tab=votes#tab-top
+    let APICalendarEvents = ["sdf","sdf","324"]; // Temporary. Replace with calendar query script
+    let calendarEvents = []
+    APICalendarEvents.forEach(element => {
+      calendarEvents.push({title: "Event Title", description: "Event Description. Leedle leedle leedle leedle leedle leedle leedle leedle"})
+    });
+
+    // Update state
+    // https://reactjs.org/docs/state-and-lifecycle.html
+    this.setState({
+      date: selectedDate.toString(),
+      events: calendarEvents
+    });
+  }
+
+  render() {
+    // Get calendar events
+    let calendarEvents = []
+    this.state.events.forEach(event => {
+      calendarEvents.push(
+        <div className="calendarItem">
+          <div><b>{event.title}</b></div>
+          <div>{event.description}</div>
+        </div>
+      )
+    })
+
+    return (
+      <div className="Calendar">
+        <Calendar
+          onChange={this.getCalendarEvents}
+        />
+        <div>
+          <div className="date">{this.state.date}</div>
+          <div className="events">
+            {calendarEvents}
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
 }
 
 export default CalendarWidget;
+
+// Some other references:
+// https://www.npmjs.com/package/react-calendar
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// https://reactjs.org/docs/composition-vs-inheritance.html (maybe)
