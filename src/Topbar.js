cadee5a458abe053e77aeb258a6d0129a0c752f3
Sanlinlin <shengjia@pdx.edu>
2020-07-14 21:53:05 -0700

    delete the comment in the Topbar.js file

--- src/Topbar.js
+++ src/Topbar.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-// import Nav from 'react-bootstrap/Nav'
-// import NavDropdown from 'react-bootstrap/NavDropdown'
 import Background from './Background'
 import './BackgroundColor.css'
 
