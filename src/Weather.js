a17cddd996e9568ec676eecceec556c731632164
Sanlinlin <shengjia@pdx.edu>
2020-07-25 13:53:16 -0700

    add the weather files to the github

---
+++ src/Weather.js
@@ -0,0 +1,26 @@
+//Draggable and resizable example from:
+//https://github.com/bokuweb/react-rnd
+//Used to create draggable and resizable 'widgets'
+
+import React from "react";
+import "./App.css"
+import "./Weather.css"
+import WeatherInfo from "./WeatherInfo"
+import { Rnd } from 'react-rnd';
+
+export default function Weather(props){
+    return(
+        <Rnd
+            minWidth= {400}
+            minHeight= {400}
+            className="WeatherWidget"
+            default={{
+                x: -100,
+                y: 300,
+            }}>
+            <div className="handle">
+                <WeatherInfo/>
+            </div>
+        </Rnd>
+    );
+}
