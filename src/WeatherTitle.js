147955cf6ac770e06231dc6094f0f2a090ed431f
Sanlinlin <shengjia@pdx.edu>
2020-08-11 16:58:38 -0700

    update the some infor

---
+++ src/WeatherTitle.js
@@ -0,0 +1,21 @@
+// https://www.youtube.com/watch?v=204C9yNeOYI
+// Use this above link to do the Open Weather API as an example
+// To finish our project
+
+import React, { Component } from 'react'
+
+class WeatherTitle extends Component {
+    render() {
+        const cityStyle = {
+            fontFamily: "Cursive"
+        };
+
+        return (
+            <div>
+                <h1 style={cityStyle}>Weather Report</h1>
+            </div>
+        )
+    }
+}
+
+export default WeatherTitle
