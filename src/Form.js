2f06d53c0113c7906d49e6dd1e860d3430ca0502
Sanlinlin <shengjia@pdx.edu>
2020-08-11 15:00:46 -0700

    update some cities for users to choose

--- src/Form.js
+++ src/Form.js
@@ -21,24 +21,9 @@ class Form extends Component {
                     <option value="Chicago">Chicago</option>
                     <option value="Detroit">Detroit</option>
                     <option value="Washington D.C.">Washington D.C.</option>
-                    <option value="Boston">Boston</option>
-                    <option value="Austin">Austin</option>
-                    <option value="San Jose">San Jose</option>
-                    <option value="Denver">Denver</option>
-                    <option value="Dallas">Dallas</option>
-                    <option value="Philadelphia">Philadelphia</option>
-                    <option value="Houston">Houston</option>
-                    <option value="Baltimore">Baltimore</option>
-                    <option value="San Diego">San Diego</option>
-                    <option value="San Antonio">San Antonio</option>
-                    <option value="St. Louis">St. Louis</option>
-                    <option value="Miami">Miami</option>
-                    <option value="Memphis">Memphis</option>
-                    <option value="New Orleans">New Orleans</option>
-                    <option value="Indianapolis">Indianapolis</option>
                 </select>
                 <button style={cityStyle}>
-                    Get Weather
+                    Getting Weather
                 </button>
                 </form>
             </div>
