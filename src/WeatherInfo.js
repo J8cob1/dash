0c07babc47c6ead6c7a3461d15b0d4397b95eeac
Sanlinlin <shengjia@pdx.edu>
2020-08-11 21:33:55 -0700

    update some code

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,59 +17,48 @@ const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 console.log(process.env);
 
 class WeatherInfo extends Component {
-    state = {
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: undefined
-    }
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
 
-    getWeather = async (e) => {
-        e.preventDefault();
-    
-        const city = e.target.elements.city.value;
-        const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},us&appid=${API_KEY}&units=metric`);
-        const data = await api_call.json();
-        
-        if(city){
-          this.setState({
-            temperature: data.main.temp,
-            city: data.name,
-            country: data.sys.country,
-            humidity: data.main.humidity,
-            description: data.weather[0].description,
-            error: ''
-          });
-        } else {
-          this.setState({
-            temperature: undefined,
-            city: undefined,
-            country: undefined,
-            humidity: undefined,
-            description: undefined,
-            error: "Please enter the value ~~"
-          });
-        }
-      }
+  getWeather = async (e) => {
+    e.preventDefault();
 
-    render() {
-        return (
-            <div>
-                <WeatherTitle/>
-                    <WeatherForm getWeather={this.getWeather}/>
-                    <WeatherDetails
-                    temperature={this.state.temperature}
-                    city={this.state.city}
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}
-                />
-            </div>
-        )
-    }
+    const city = e.target.elements.city.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},us&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+  
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ''
+    });
+  }
+
+  render() {
+      return (
+          <div>
+              <WeatherTitle/>
+                  <WeatherForm getWeather={this.getWeather}/>
+                  <WeatherDetails
+                  temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  humidity={this.state.humidity}
+                  description={this.state.description}
+                  error={this.state.error}
+              />
+          </div>
+      )
+  }
 }
 
 export default WeatherInfo
